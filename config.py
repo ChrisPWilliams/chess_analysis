31cc37394d8e2057d21a6c4726e88d9aba9d1fe7
Chris Williams <orktagonapus@gmail.com>
2022-09-09 16:48:37 +0100

    minor refactoring while writing test script, time for docker setup

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ env_vars = os.environ.items()
 
 class MYSQL:
     USER=env_vars['USER']
-    PORT=env_vars['PORT'] or 3000
+    PORT=env_vars['PORT'] or 3307
     DATABASE=env_vars['DATABASE']
     PASSWORD=env_vars['DB_PASS']
     HOST=env_vars['HOST']
