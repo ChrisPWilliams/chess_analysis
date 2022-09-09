ac3ef865cb9ccc2c04eba4d6a7b27fd658850b52
Chris Williams <orktagonapus@gmail.com>
2022-09-09 17:41:58 +0100

    minor refactoring while writing test script, time for docker setup

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
+from db_connection_defaults import default_connectors   #local .py file for setting environment variable defaults, create your own if cloning
 
 env_vars = os.environ.items()
 
 class MYSQL:
-    USER=env_vars['USER']
-    PORT=env_vars['PORT'] or 3307
-    DATABASE=env_vars['DATABASE']
-    PASSWORD=env_vars['DB_PASS']
-    HOST=env_vars['HOST']
+    USER=env_vars['DB_USER'] or default_connectors.USER
+    PORT=env_vars['PORT'] or default_connectors.PORT
+    DATABASE=env_vars['DATABASE'] or default_connectors.DATABASE
+    PASSWORD=env_vars['DB_PASS'] or default_connectors.DB_PASS
+    HOST=env_vars['DB_HOST'] or default_connectors.HOST
