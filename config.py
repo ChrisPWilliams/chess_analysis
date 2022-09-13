c51321dcb65c73565263461988b52de17913b37d
Chris Williams <orktagonapus@gmail.com>
2022-09-13 12:40:57 +0100

    refresh script tested and functioning

--- config.py
+++ config.py
@@ -9,4 +9,14 @@ class MYSQL:
     HOST=os.environ.get('DB_HOST') or default_connectors.HOST
 
 class CHESS_DOT_COM:
-    USER=default_connectors.CHESS_USER
+    USER=default_connectors.CHESS_USER 
+
+# to run this locally, use the following default connectors to get everything set up. Create a db_connection_defaults.py file with the following structure:
+
+# class default_connectors():
+#     USER = 'your database username'
+#     DB_PASS = 'your database password'
+#     DATABASE = 'your database name'
+#     PORT = 3306               # default port for mysql, if you have set up your server with a different port use that one
+#     HOST = 'localhost'
+#     CHESS_USER = 'your chess.com username'
